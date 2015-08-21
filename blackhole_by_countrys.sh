60dd184f0abbb83f7540672d6a7edb951f5e3da8
buka <buka@support2.webdev>
2015-08-21 15:40:59 +0300

    isyntax

--- blackhole_by_countrys.sh
+++ blackhole_by_countrys.sh
@@ -9,36 +9,36 @@ SCOUNTRY=$(dialog --stdout --title "list" --cancel-label "new" --checklist "sele
 STYPE=$(dialog --stdout --title "list" --cancel-label "new" --menu "select type:" 15 55 5 ${TYPE} )
 
 if [ -z ${SCOUNTRY} ] ;then 
- echo kk
+echo kk
 fi
 
 
 for icountry in ${SCOUNTRY} ; do 
 
-  ## type1 USE blackhole 
-  #
-  if [ "blk" == ${STYPE} ];then 
-     wget -O -  "https://stat.ripe.net/data/country-resource-list/data.json?resource=${icountry}&v4_format=prefix" | ./jq '."data"."resources"."ipv4"[]' 2>&1 | xargs -l1 ip ro add blackhole 
-  
-  fi 
-  
-  ## type2 USE raw list
-  #
-  if [ "list" == ${STYPE} ];then 
-     wget -O -  "https://stat.ripe.net/data/country-resource-list/data.json?resource=${icountry}&v4_format=prefix" | ./jq '."data"."resources"."ipv4"[]' 2>&1 | xargs -l1 echo 
-  
-  fi 
-  
-  ## type3 USE iptables 
-  #
-  if [ "iptab" == ${STYPE} ];then 
-     iptables -N ban_${icountry}
-     LIST2=`wget -O -  "https://stat.ripe.net/data/country-resource-list/data.json?resource=${icountry}&v4_format=prefix" | ./jq '."data"."resources"."ipv4"[]' 2>&1 `
-     for IPS2 in ${LIST2};do 
-       iptables -A ban_${icountry} -s ${IPS2} -j DROP 
-     done
-  
-    iptables -I INPUT -j ban_${icountry}
+## type1 USE blackhole 
+#
+if [ "blk" == ${STYPE} ];then 
+wget -O -  "https://stat.ripe.net/data/country-resource-list/data.json?resource=${icountry}&v4_format=prefix" | ./jq '."data"."resources"."ipv4"[]' 2>&1 | xargs -l1 ip ro add blackhole 
+
+fi 
+
+## type2 USE raw list
+#
+if [ "list" == ${STYPE} ];then 
+wget -O -  "https://stat.ripe.net/data/country-resource-list/data.json?resource=${icountry}&v4_format=prefix" | ./jq '."data"."resources"."ipv4"[]' 2>&1 | xargs -l1 echo 
+
+fi 
+
+## type3 USE iptables 
+#
+if [ "iptab" == ${STYPE} ];then 
+iptables -N ban_${icountry}
+LIST2=`wget -O -  "https://stat.ripe.net/data/country-resource-list/data.json?resource=${icountry}&v4_format=prefix" | ./jq '."data"."resources"."ipv4"[]' 2>&1 `
+for IPS2 in ${LIST2};do 
+iptables -A ban_${icountry} -s ${IPS2} -j DROP 
+done
+
+iptables -I INPUT -j ban_${icountry}
 
 fi 
 
